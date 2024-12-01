ae832c5eb801376723479da511d1bd7962e6aedf
Junior <odiseoj676@gmail.com>
2024-12-01 02:13:44 -0600

    Refactor UserModel to use None as default for optional attributes

--- models/User.py
+++ models/User.py
@@ -4,17 +4,17 @@ from datetime import datetime
 class UserModel:
     def __init__(self, data):
         self.nombre = data.get('nombre', '')
-        self.apellido = data.get('apellido', '')
-        self.fecha_nacimiento = data.get('fecha_nacimiento', '')
+        self.apellido = data.get('apellido', None)
+        self.fecha_nacimiento = data.get('fecha_nacimiento', None)
         self.email = data.get('email', '')
         self.password = data.get('password', '')
-        self.telefono = data.get('telefono', '')
+        self.telefono = data.get('telefono', None)
         self.tipoSuscripcion = data.get('tipoSuscripcion', '') ## Ej: "básica", "premium"
         self.rol = data.get('rol', '') ## Ej: "admin", "ganadero"
-        self.direccion = data.get('direccion', '')
-        self.image = data.get('image', '')
+        self.direccion = data.get('direccion', None)
+        self.image = data.get('image', None)
         self.create_at = data.get('create_at', datetime.now())
         self.update_at = data.get('update_at', datetime.now())
-        self.fincaId = data.get('fincaId', '')
-        self.pushToToken = data.get('pushToToken', '')
+        self.fincaId = data.get('fincaId', None)
+        self.pushToToken = data.get('pushToToken', None)
         self.status = data.get('status', '')
